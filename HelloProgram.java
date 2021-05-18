6fd4071f5aee2c60f3156db65853cc5755673642
Santosh Kumar <48355333+santosrk@users.noreply.github.com>
2021-05-18 17:18:13 +0530

    Update HelloProgram.java

--- HelloProgram.java
+++ HelloProgram.java
@@ -2,8 +2,8 @@ public class HelloProgram {
     public static void main(String[] s){
         System.out.println("Loop based execution - CICD Demo");
 
-        for(int i=0; i<50;i++){
-            System.out.println(i);
+        for(int i=0; i<5;i++){
+            System.out.println("Santosh :" +i);
         }
     }
 }
